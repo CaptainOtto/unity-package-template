d7fa864fa7bc1753a0d7668dc14af1c50b2f2dc7
Johan Ottosson <jottossonta@gmail.com>
2022-07-28 12:20:44 +0200

    Create EditorExample.cs

---
+++ Editor/EditorExample.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+using System.Collections.Generic;
+
+namespace CaptnJohn.Template.Editor
+{
+    /// <summary>
+    /// Placeholder example script.
+    /// </summary>
+    /// <seealso cref="RuntimeExample"/>
+    [CustomEditor(typeof(RuntimeExample))]
+    public class EditorExample : UnityEditor.Editor
+    {
+    }
+}
