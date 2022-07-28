d7d14e69346e94dc465e3f13cc6d95e14064aa0c
Johan Ottosson <jottossonta@gmail.com>
2022-07-28 12:14:27 +0200

    Added RuntimeExample example cs file.

---
+++ RuntimeExample.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace CaptnJohn.Template 
+{
+    /// <summary>
+    /// This is an example script for starting a package.
+    /// </summary>
+    public class RuntimeExample : MonoBehaviour 
+    {
+        void Start() {} 
+        void Update() {}
+    }
+}
